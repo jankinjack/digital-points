69f718d6f7b4bd6d89e4e2f50cc9842158f2f081
jankinjack <n.sevostyanov97@yandex.ru>
2026-07-17 19:02:41 +0700

    Release v0.1.3.

--- server/inc/micro_dp.h
+++ server/inc/micro_dp.h
@@ -22,7 +22,7 @@
 
 #define DP_VERSION_MAJOR ((uint16_t)0)
 #define DP_VERSION_MINOR ((uint16_t)1)
-#define DP_VERSION_PATCH ((uint16_t)2)
+#define DP_VERSION_PATCH ((uint16_t)3)
 
 // Export defines for DLLs.
 #ifdef MICRO_DP_EXPORTS
